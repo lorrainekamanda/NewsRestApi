f0257ea925a956c730587b5daa0d0fb13fc7f709
Rain <user@localhost.localdomain>
2022-03-24 12:40:38 -0400

    add Login view api

--- newsboardapi/views.py
+++ newsboardapi/views.py
@@ -5,6 +5,8 @@ from .serializers import RegisterSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed
+import jwt
+import datetime
 
 # Create your views here.
 class RegisterView(APIView):
@@ -29,7 +31,20 @@ class LoginView(APIView):
         if not user.check_password(password):
             raise  AuthenticationFailed('Incorrect Password')
 
-        return Response({
-            "message":"success"
-        })
+        payload ={
+            'id':user.id,
+            'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes = 60),
+            'iat':datetime.datetime.utcnow()
+        }
+        token = jwt.encode(payload,'secret',algorithm ='HS256')
+        response = Response()
+        response.set_cookie(key='jwt',value = token,httponly = True)
+
+        response.data={
+            "jwt":token,
+            'name': user.name,
+            'email': user.email
+        }
+
+        return response
 
--- newsboardproject/settings.py
+++ newsboardproject/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'newsboardapi',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,3 +133,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'newsboardapi.User'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
