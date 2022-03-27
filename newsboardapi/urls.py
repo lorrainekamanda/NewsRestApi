4c10c4768e5299e35b22718ecd9ca82effb080b2
Rain <user@localhost.localdomain>
2022-03-27 07:42:43 -0400

    set register as home page

--- newsboardapi/urls.py
+++ newsboardapi/urls.py
@@ -5,9 +5,9 @@ app_name = "newsboardapi"
 
 urlpatterns = [
     
-    path('register/',RegisterView.as_view(),name = "register"),
+    path('',RegisterView.as_view(),name = "register"),
     path('login/',LoginView.as_view(),name = "login"),
-    path('',PostApiView.as_view(),name='posts_list'),
+    path('posts-list/',PostApiView.as_view(),name='posts_list'),
     path('posts-detail/<pk>/',PostDetailView.as_view(),name='posts_detail'),
     path('comments', CommentListsView.as_view(), name='comments'),
     path('comments/<pk>', CommentDetailView.as_view(), name='comment_details'),
